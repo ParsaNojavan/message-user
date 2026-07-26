04245e13ebb424dfa4bb78fef62f63cc7486a193
parsanojavan <parsa.nojavan85@gmail.com>
2026-07-26 20:31:07 +0330

    feat(user): add user auth proccess

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { UserModule } from './user.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(UserModule, {
     transport: Transport.REDIS,
     options: {
       host: 'localhost',
